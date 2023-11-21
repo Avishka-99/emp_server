4887bfa26c331c610c9992a7c7f21544c81040c4
Avishka prabhath <avishkaprabha360@gmail.com>
2023-11-21 16:50:00 +0530

    changes of modals

--- emp_server/Models/Products.cs
+++ emp_server/Models/Products.cs
@@ -2,7 +2,7 @@
 {
     public class Products
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public float Price { get; set; }
         public int Quantity { get; set; }
